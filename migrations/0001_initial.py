0d8666fd77ca7a8a61bddaabd59a5b64a7a34ca3
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2018-02-02 22:38:15 +0100

    added django migrations

---
+++ migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Server',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('ranking_last_update', models.DateTimeField()),
+                ('ranking_outdated', models.BooleanField(default=False)),
+            ],
+        ),
+    ]
