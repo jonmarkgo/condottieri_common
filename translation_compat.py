2c0fe0bd9f73a768c7bbc1842e3e325abc286d44
Jon Gottfried <jonmarkgo@gmail.com>
2025-03-17 20:43:02 +0000

    updates

--- translation_compat.py
+++ translation_compat.py
@@ -2,4 +2,8 @@ from django.utils.translation import gettext_noop, gettext_lazy
 
 # Compatibility layer for old translation functions
 ugettext_noop = gettext_noop
-ugettext_lazy = gettext_lazy 
+ugettext_lazy = gettext_lazy
+
+# Monkey-patch Django's translation module to provide ugettext_lazy
+import django.utils.translation
+django.utils.translation.ugettext_lazy = gettext_lazy 
